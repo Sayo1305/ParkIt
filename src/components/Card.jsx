6b13eef3c638095251c9bfc1c15aca588a8f46b7
Unnat Das <udas4153@gmail.com>
2023-06-06 00:23:06 +0530

    UI corrected

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Card = ({data}) => {
-  // console.log(data)
   const navigate = useNavigate();
   return (
-    <div className='h-full w-full cursor-pointer p-3 shadow-lg bg-blue-800 rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20   ' onClick={()=>{
+    <div className='h-full w-full flex flex-col gap-2 cursor-pointer p-3 shadow-lg bg-[#74c5cd] rounded-lg ' onClick={()=>{
       localStorage.setItem("owner_wallet_id" , data.Walletaddress);
       navigate(`/Placedetail/${data.id}`);
     }}>
-      <div><img className='w-full h-[100px] object-cover' src={data.img_} alt="djd" /></div>
-      <div>{data.area}</div>
-      <div>{data.place}</div>
-      <div>{data.pincode}</div>
-      <div className='bg-red-200 p-1 text-center rounded-lg cursor-pointer'>Rent Now</div>
+      <div><img className='w-full rounded-lg h-[200px] object-cover' src={data.image} alt="djd" /></div>
+      <div className='w-full text-lg text-center font-semibold'>{data.area}</div>
+      <div className='text-center w-full'>{data.place} | {data.pincode}</div>
+      <div className='bg-green-500 border border-black p-1 text-center rounded-lg cursor-pointer'>Rent Now</div>
     </div>
   )
 }
--- src/pages/Placedetails.jsx
+++ src/pages/Placedetails.jsx
@@ -1,25 +1,64 @@
-import { getDatabase, onValue, ref } from 'firebase/database';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { getDatabase, onValue, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Placedetails = () => {
-      const db = getDatabase();
-      const [dataset , setdataset] = useState([]);
-      const {id : primary_id} = useParams();
-      const owner_wallet_id = localStorage.getItem("owner_wallet_id");
-      useEffect(()=>{
-            let temparr = [];
-            onValue(ref( db, `/${owner_wallet_id}/${primary_id}/`) , (snapshot)=>{
-                        temparr.push(snapshot.val());
-            })
-            console.log(temparr);
-            setdataset(temparr);
-      },[]);
+  const db = getDatabase();
+  const [dataset, setdataset] = useState([]);
+  const { id: primary_id } = useParams();
+  const owner_wallet_id = localStorage.getItem("owner_wallet_id");
+  const [timmingdata, settimmingdata] = useState([]);
+  useEffect(() => {
+    let temparr = [];
+    onValue(ref(db, `/${owner_wallet_id}/${primary_id}/`), (snapshot) => {
+      temparr.push(snapshot.val());
+    });
+    let arr = [];
+    temparr[0].days.map((val) => {
+      if (val.select_ === true) {
+        const val_data = Object.keys(val.timing);
+        for (var i = 0; i < val_data.length; i++) {
+          // console.log(val.timing[val_data[i]])
+          if(val.timing[val_data[i]] === true)
+          {
+            // console.log(val.timing)
+            arr.push(val.timing[val_data])
+          }
+        }
+      }
+    });
+    console.log(arr);
+    setdataset(temparr[0]);
+  }, []);
   return (
-    <div>
-      place detail make page fetch by {primary_id}
+    <div className="w-full min-h-screen bg-green-200">
+      <div className="w-full h-[400px] object-contain bg-black shadow-2xl relative">
+        <img
+          className="w-full h-full object-contain object-center"
+          src={dataset?.image}
+          alt=""
+        />
+      </div>
+      <div className="w-full text-center text-6xl font-semibold">
+        {dataset?.place}
+      </div>
+      <div className="flex p-5 w-full justify-center text-2xl items-center gap-10">
+        <div>{dataset?.area}</div>|<div>{dataset?.pincode}</div>
+      </div>
+      <div className="p-2 font-semibold text-2xl">Available Date</div>
+      <div className="flex flex-wrap p-3 gap-5">
+        {dataset?.days?.map((val) => (
+          <>
+            {val.select_ === true && (
+              <div className="p-2 cursor-pointer bg-green-600 rounded-lg text-white">
+                {val.day}
+              </div>
+            )}
+          </>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Placedetails
+export default Placedetails;
